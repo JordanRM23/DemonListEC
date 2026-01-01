afb2b77ab626618a942bfcb674717a7a36caeb3c
JordanRM23 <157156538+JordanRM23@users.noreply.github.com>
2026-01-01 08:09:20 -0500

    fix?

--- js/routes.js
+++ js/routes.js
@@ -1,13 +1,9 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import submit from './pages/submit.js';
-import login from './pages/login.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/submit', component: submit },
-    { path: '/login', component: login },
 ];
