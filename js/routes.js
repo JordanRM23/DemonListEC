4d94553786d6163c03e5ce9ce718ded063384e5f
JordanRM23 <157156538+JordanRM23@users.noreply.github.com>
2026-01-01 08:05:14 -0500

    index.html, login.ccs, routes.js, submitrecord.js - test

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,13 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import submit from './pages/submit.js';
+import login from './pages/login.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/submit', component: submit },
+    { path: '/login', component: login },
 ];
