262bff29b78f846a1e25e7d1e96e266bd1070814
JordanRM23 <157156538+JordanRM23@users.noreply.github.com>
2026-01-17 10:53:19 -0500

    arreglate mierda

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import Changelog from './pages/Changelog.js'
+import Changelog from './pages/changelog.js'
 
 export default [
     { path: '/', component: List },
