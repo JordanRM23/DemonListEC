21f454544a5cabbdbdc61cc3e2822d0df2486357
JordanRM23 <157156538+JordanRM23@users.noreply.github.com>
2026-02-01 11:36:46 -0500

    test

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -5,6 +5,13 @@ export default {
         </main>
         <div v-else class="page-changelog">
             <div class="change">
+                <h2>01/02/2026</h2>
+                <p>Manic Machine has place at #91, above Libertas and below Niflheim, This Pushes Insaction into the Legacy List</p>
+                <p>Stellaluna has place at #115, above Tech Manifestation and below Broken Signal, This Pushes The Lost Existence into the Legacy List</p>
+                <p>EKO has place at #80, above cytokinesis and below Triple Six, This Pushes Forbidden Isle into the Legacy List</p>
+                <p>Untitled has place at #68, above Epsilon and below Carcano, This Pushes 8o X into the Legacy List</p>
+                <p>Conical Depression has place at #77, above Goldcrest Palace and below Auroral Valhalla, This Pushes Dark Odyssey into the Legacy List</p>
+
                 <h2>31/01/2026</h2>
                 <p>Se añadió la pestaña "Changelog". Los registros de cambios anteriores no están disponibles.</p>
                 <p>Esta página mostrará todos los cambios futuros en las listas, así como los cambios en el formato o las reglas de la lista.</p>
