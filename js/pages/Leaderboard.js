590a74b5530ce31931fc1cf547a3f88dd45e1f30
milo <fmdoeringtt@gmail.com>
2026-02-06 22:19:44 -0500

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -212,7 +212,7 @@ export default {
 
         getPlayerRole(user) {
             const roles = {
-                'Zephyr': ['La cabra del six seven' , 'Main List Record'],
+                'Zephyr': 'La cabra del six seven',
                 'venomioo': 'EX-ECDL',
                 'Locked': 'EX-ECDL',
                 'RaymanNinja': 'EX-ECDL',
