171f948eb0fc973ab5349ee61f4c008e7c96585d
milo <fmdoeringtt@gmail.com>
2026-02-21 17:04:07 -0500

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -308,13 +308,14 @@ export default {
             };
         },
 
-        getLevelPosClass(rank) {
-            rank = Number(rank) || 0;
+      getLevelPosClass(rank) {
+    rank = Number(rank) || 0;
 
-            if (rank >= 1 && rank <= 50) return 'level-pos-top50';
-            if (rank >= 51 && rank <= 100) return 'level-pos-51-100';
-            if (rank >= 101 && rank <= 150) return 'level-pos-101-150';
-            return 'level-pos-151plus';
+    if (rank >= 1 && rank <= 75) return 'level-pos-top50';
+    if (rank >= 76 && rank <= 150) return 'level-pos-51-100';
+    if (rank >= 151) return 'level-pos-101-150';
+    return 'level-pos-151plus';
+},
         },
 
         getNameClass(total) {
